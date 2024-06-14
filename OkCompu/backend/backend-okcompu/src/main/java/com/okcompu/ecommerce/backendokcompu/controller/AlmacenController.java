a707745a46b3c44a7e45a17c53aa8039a19a21d7
ehuacachi <ehuacachi@gmail.com>
2024-06-14 07:34:20 -0500

    Clase Generica de ModelMapper

--- OkCompu/backend/backend-okcompu/src/main/java/com/okcompu/ecommerce/backendokcompu/controller/AlmacenController.java
+++ OkCompu/backend/backend-okcompu/src/main/java/com/okcompu/ecommerce/backendokcompu/controller/AlmacenController.java
@@ -3,9 +3,10 @@ package com.okcompu.ecommerce.backendokcompu.controller;
 import com.okcompu.ecommerce.backendokcompu.dto.AlmacenDTO;
 import com.okcompu.ecommerce.backendokcompu.model.Almacen;
 import com.okcompu.ecommerce.backendokcompu.service.AlmacenService;
+import com.okcompu.ecommerce.backendokcompu.util.MapperUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,35 +23,40 @@ import java.util.List;
 public class AlmacenController {
 
     private final AlmacenService almacenService;
-    @Qualifier("defaultMapper")
-    private final ModelMapper mapper;
+//    @Qualifier("defaultMapper")
+//    private final ModelMapper mapper;
+    private final MapperUtil mapperUtil;
 
     @GetMapping
     public ResponseEntity<List<AlmacenDTO>> findAll() {
-        List<AlmacenDTO> dtos = almacenService.findAll().stream().map(this::converDTO).toList();
-        return ResponseEntity.ok(dtos);
+//        List<AlmacenDTO> dtos = almacenService.findAll().stream().map(this::converDTO).toList();
+        List<AlmacenDTO> list = mapperUtil.mapList(almacenService.findAll(), AlmacenDTO.class);
+        return ResponseEntity.ok(list);
     }
     @GetMapping("/page/{page}")
     public ResponseEntity<Page<AlmacenDTO>> findAllPage(@PathVariable Integer page) {
-        Page<AlmacenDTO> dtos = almacenService.findAll(PageRequest.of(page,4 )).map(this::converDTO);
+        Page<AlmacenDTO> dtos = almacenService.findAll(PageRequest.of(page,4 )).map(a -> mapperUtil.map(a, AlmacenDTO.class));
         return ResponseEntity.ok(dtos);
     }
     @GetMapping("/{id}")
     public ResponseEntity<AlmacenDTO> findById(@PathVariable("id") Long id) {
         Almacen obj = almacenService.findById(id);
-        return ResponseEntity.ok(converDTO(obj));
+//        return ResponseEntity.ok(converDTO(obj));
+        return ResponseEntity.ok(mapperUtil.map(obj, AlmacenDTO.class));
     }
     @PostMapping
     public ResponseEntity<Void> save(@Valid @RequestBody AlmacenDTO dto) {
-        Almacen obj = almacenService.create(convertEntity(dto));
+//        Almacen obj = almacenService.create(convertEntity(dto));
+        Almacen obj = almacenService.create(mapperUtil.map(dto, Almacen.class));
         URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/id").buildAndExpand(obj.getIdAlmacen()).toUri();
         return  ResponseEntity.created(location).build();
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<AlmacenDTO> update(@PathVariable Long id,@Valid @RequestBody AlmacenDTO dto) {
-        Almacen obj = almacenService.update(id,convertEntity(dto));
-        return  ResponseEntity.ok(converDTO(obj));
+//        Almacen obj = almacenService.update(id,convertEntity(dto));
+        Almacen obj = almacenService.update(id,mapperUtil.map(dto, Almacen.class));
+        return  ResponseEntity.ok(mapperUtil.map(obj, AlmacenDTO.class));
     }
 
     @DeleteMapping("{id}")
@@ -59,11 +65,11 @@ public class AlmacenController {
         return ResponseEntity.noContent().build();
     }
 
-    private Almacen convertEntity(AlmacenDTO dto) {
-        return mapper.map(dto, Almacen.class);
-    }
-
-    private AlmacenDTO converDTO(Almacen almacen) {
-        return mapper.map(almacen, AlmacenDTO.class);
-    }
+//    private Almacen convertEntity(AlmacenDTO dto) {
+//        return mapper.map(dto, Almacen.class);
+//    }
+//
+//    private AlmacenDTO converDTO(Almacen almacen) {
+//        return mapper.map(almacen, AlmacenDTO.class);
+//    }
 }
--- OkCompu/backend/backend-okcompu/src/main/java/com/okcompu/ecommerce/backendokcompu/controller/EmpleadoController.java
+++ OkCompu/backend/backend-okcompu/src/main/java/com/okcompu/ecommerce/backendokcompu/controller/EmpleadoController.java
@@ -3,6 +3,7 @@ package com.okcompu.ecommerce.backendokcompu.controller;
 import com.okcompu.ecommerce.backendokcompu.dto.EmpleadoDTO;
 import com.okcompu.ecommerce.backendokcompu.model.Empleado;
 import com.okcompu.ecommerce.backendokcompu.service.EmpleadoService;
+import com.okcompu.ecommerce.backendokcompu.util.MapperUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -23,35 +24,39 @@ public class EmpleadoController {
 
     private final EmpleadoService empleadoService;
 
-    @Qualifier("empleadoMapper")
-    private final ModelMapper mapper;
+//    @Qualifier("empleadoMapper")
+//    private final ModelMapper mapper;
+
+    private final MapperUtil mapperUtil;
 
     @GetMapping
     public ResponseEntity<List<EmpleadoDTO>> findAll() {
-        List<EmpleadoDTO> dtos = empleadoService.findAll().stream().map(this::converDTO).toList();
+//        List<EmpleadoDTO> dtos = empleadoService.findAll().stream().map(this::converDTO).toList();
+        List<EmpleadoDTO> dtos = mapperUtil.mapList(empleadoService.findAll(), EmpleadoDTO.class, "empleadoMapper");
         return ResponseEntity.ok(dtos);
     }
     @GetMapping("/page/{page}")
     public ResponseEntity<Page<EmpleadoDTO>> findAllPage(@PathVariable Integer page) {
-        Page<EmpleadoDTO> dtos = empleadoService.findAll(PageRequest.of(page,4 )).map(this::converDTO);
+//        Page<EmpleadoDTO> dtos = empleadoService.findAll(PageRequest.of(page,4 )).map(this::converDTO);
+        Page<EmpleadoDTO> dtos = empleadoService.findAll(PageRequest.of(page,4 )).map(e -> mapperUtil.map(e, EmpleadoDTO.class, "empleadoMapper"));
         return ResponseEntity.ok(dtos);
     }
     @GetMapping("/{id}")
     public ResponseEntity<EmpleadoDTO> findById(@PathVariable("id") Long id) {
         Empleado obj = empleadoService.findById(id);
-        return ResponseEntity.ok(converDTO(obj));
+        return ResponseEntity.ok(mapperUtil.map(obj, EmpleadoDTO.class, "empleadoMapper"));
     }
     @PostMapping
     public ResponseEntity<Void> save(@Valid @RequestBody EmpleadoDTO dto) {
-        Empleado obj = empleadoService.create(convertEntity(dto));
+        Empleado obj = empleadoService.create(mapperUtil.map(dto, Empleado.class, "empleadoMapper"));
         URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/id").buildAndExpand(obj.getIdEmpleado()).toUri();
         return  ResponseEntity.created(location).build();
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<EmpleadoDTO> update(@PathVariable Long id,@Valid @RequestBody EmpleadoDTO dto) {
-        Empleado obj = empleadoService.update(id,convertEntity(dto));
-        return  ResponseEntity.ok(converDTO(obj));
+        Empleado obj = empleadoService.update(id,mapperUtil.map(dto, Empleado.class, "empleadoMapper"));
+        return  ResponseEntity.ok(mapperUtil.map(obj, EmpleadoDTO.class, "empleadoMapper"));
     }
 
     @DeleteMapping("{id}")
@@ -60,11 +65,11 @@ public class EmpleadoController {
         return ResponseEntity.noContent().build();
     }
 
-    private Empleado convertEntity(EmpleadoDTO dto) {
-        return mapper.map(dto, Empleado.class);
-    }
-
-    private EmpleadoDTO converDTO(Empleado empleado) {
-        return mapper.map(empleado, EmpleadoDTO.class);
-    }
+//    private Empleado convertEntity(EmpleadoDTO dto) {
+//        return mapper.map(dto, Empleado.class);
+//    }
+//
+//    private EmpleadoDTO converDTO(Empleado empleado) {
+//        return mapper.map(empleado, EmpleadoDTO.class);
+//    }
 }
